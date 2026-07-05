c5a40674de6bd918b0d7151efddeeda0971a42cf
hyptryx <gharry1979@gmail.com>
2026-07-05 18:20:51 +0200

    Create community.js

---
+++ community.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const fill = document.getElementById("progressFill");
+
+    if(!fill) return;
+
+    const percent = 68;
+
+    setTimeout(()=>{
+
+        fill.style.width = percent + "%";
+
+    },500);
+
+});
