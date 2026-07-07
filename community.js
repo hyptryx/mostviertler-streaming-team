101ed7ba7005b3b051167b868a4f47b9034e8348
hyptryx <gharry1979@gmail.com>
2026-07-07 23:19:15 +0200

    Update community.js

--- community.js
+++ community.js
@@ -8,6 +8,21 @@
    FIREBASE
 ========================================== */
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAhdTxRsm0qNUTw-iN8AHJI5ZYSA2m9oII",
+    authDomain: "mosti-catch.firebaseapp.com",
+    databaseURL: "https://mosti-catch-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mosti-catch",
+    storageBucket: "mosti-catch.appspot.com",
+    messagingSenderId: "100463668785",
+    appId: "1:100463668785:web:3e4bb19ea88a99905d6f10"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.database();
 
 
 /* ==========================================
