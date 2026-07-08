8ea30ef9320c621506b32587febf47e4b96abfda
hyptryx <gharry1979@gmail.com>
2026-07-08 23:03:30 +0200

    Update premium-effects.js

--- premium-effects.js
+++ premium-effects.js
@@ -1,4 +1,27 @@
 /* =====================================================
-   MostiFX Engine v1.0
+   MostiFX Engine v0.1
+   Mostviertler Streaming Team
 ===================================================== */
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Canvas erzeugen
+    const canvas = document.createElement("canvas");
+    canvas.id = "mostifx-canvas";
+
+    document.body.prepend(canvas);
+
+    const ctx = canvas.getContext("2d");
+
+    function resize(){
+
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+    }
+
+    resize();
+
+    window.addEventListener("resize", resize);
+
+});
