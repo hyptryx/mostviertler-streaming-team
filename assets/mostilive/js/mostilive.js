bf5a45a7ee13adb409b4d4d64e77778ed06a0855
hyptryx <gharry1979@gmail.com>
2026-07-10 23:17:32 +0200

    Update mostilive.js

--- assets/mostilive/js/mostilive.js
+++ assets/mostilive/js/mostilive.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const button = document.getElementById("simulateBtn");
+
+    button.addEventListener("click", () => {
+
+        alert("🚀 Willkommen bei Mosti Live!\n\nDeveloper Mode erfolgreich gestartet.");
+
+    });
+
+});
