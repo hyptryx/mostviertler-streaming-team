b7849d0be5d08712d48b503048a000d5d6b9eab2
hyptryx <gharry1979@gmail.com>
2026-07-12 21:43:46 +0200

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,5 +1,65 @@
 // =====================================
-// Befehle
+// MostiBot Befehle
 // =====================================
 
-const COMMANDS = {};
+const COMMANDS = {
+
+    "!commands": showCommands,
+
+    "!party": partyCommand,
+
+    "!bier": beerCommand,
+
+    "!dj": djCommand
+
+};
+
+// ----------------------------
+
+function showCommands(user) {
+
+    sendBotMessage(
+`📜 Verfügbare Befehle:
+
+🎉 !party
+🍺 !bier
+🎧 !dj`
+    );
+
+}
+
+// ----------------------------
+
+function partyCommand(user) {
+
+    sendBotMessage(`🎉 ${user} startet die Party!`);
+
+}
+
+// ----------------------------
+
+function beerCommand(user) {
+
+    sendBotMessage(`🍺 Prost ${user}!`);
+
+}
+
+// ----------------------------
+
+function djCommand(user) {
+
+    const djStatus =
+        document.getElementById("dj-status");
+
+    let dj = "Kein DJ";
+
+    if (djStatus &&
+        djStatus.style.display !== "none") {
+
+        dj = djStatus.innerText;
+
+    }
+
+    sendBotMessage(`🎧 ${dj}`);
+
+}
