1ac25e57bbef5aa9a24b54e23e20d1e500f4e77c
hyptryx <gharry1979@gmail.com>
2026-07-12 22:18:08 +0200

    Update responses.js

--- js/responses.js
+++ js/responses.js
@@ -1,17 +1,26 @@
 // =====================================
-// MostiBot Konfiguration
+// MostiBot Antworten
 // =====================================
 
-const BOT = {
+const RESPONSES = {
 
-    // Bot-Informationen
-    name: "🤖 MostiBot",
-    version: "1.0",
+    party: [
 
-    // Entwicklung
-    debug: true,
+        "🎉 {user} eröffnet die Party! 🔥",
+        "🥳 Jetzt geht's rund! {user} bringt Stimmung ins MostiRadio!",
+        "🎧 Die Tanzfläche gehört jetzt {user}!",
+        "🍏 Party-Modus aktiviert von {user}!",
+        "🎊 Achtung! {user} sorgt für gute Laune!"
+    ],
 
-    // Cooldown in Millisekunden
-    cooldown: 10000
+    bier: [
+
+        "🍺 Prost {user}! Auf einen tollen Abend!",
+        "🍻 {user} bestellt eine Runde für alle!",
+        "🍺 Das erste Bier geht auf {user}!",
+        "🍻 Zum Wohl {user}! Genießt den Abend!",
+        "🍺 {user} zapft das nächste Fass an!"
+
+    ]
 
 };
