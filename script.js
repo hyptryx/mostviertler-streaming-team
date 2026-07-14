b73d7a375a4ab1538dc354be5ce994c91e61e830
hyptryx <gharry1979@gmail.com>
2026-07-14 23:12:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -667,25 +667,6 @@ function renderDropHighscores() {
 renderDropHighscores();
 
 /* ---------------------------------------------------
-   EVENT POPUP – AUTO OPEN
---------------------------------------------------- */
-
-window.addEventListener("load", () => {
-  // Nur anzeigen, wenn noch nicht gesehen
-  if (!sessionStorage.getItem("eventPopupSeen")) {
-    document.getElementById("event-popup-overlay").style.display = "block";
-    document.getElementById("event-popup").classList.add("show");
-    sessionStorage.setItem("eventPopupSeen", "true");
-  }
-});
-
-// Schließen
-document.getElementById("event-popup-close").addEventListener("click", () => {
-  document.getElementById("event-popup-overlay").style.display = "none";
-  document.getElementById("event-popup").classList.remove("show");
-});
-
-/* ---------------------------------------------------
    MOSTIRADIO – PLAYER
 --------------------------------------------------- */
 
