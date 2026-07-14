da7b938fbe112fd88be0422938c1e9e49a30e459
hyptryx <gharry1979@gmail.com>
2026-07-14 16:50:07 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ window.BOT = {
     name: "🤖 MostiBot",
     version: "1.0.0",
     debug: true,
-    developerMode: false,   // 👈 NEU
+    developerMode: true,   // 👈 NEU
     cooldown: 10000,
 
     startupFinished: false
